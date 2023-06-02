851233ca7c27163fae372efa804cf82ca6e7d605
梁渭 <liangwei.lw@nonolive.com>
2023-06-02 12:00:11 +0800

    add print(vars(c))

--- dreambooth/train_dreambooth.py
+++ dreambooth/train_dreambooth.py
@@ -736,6 +736,7 @@ def main(args: DreamboothConfig, use_txt2img: bool = True) -> TrainResult:
                                     c.seed = seed
                                     g_cuda = torch.Generator(device=accelerator.device).manual_seed(seed)
                                     if len(c.prompt) > 0:
+                                        print(vars(c))
                                         print(c.prompt)
                                         vec_prompts = json.loads(c.prompt)
                                         vec_negprompts = json.loads(c.negative_prompt)
