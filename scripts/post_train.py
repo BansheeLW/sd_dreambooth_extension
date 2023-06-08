89eb465a8356e035f9f1750fdd9d05b043f35848
梁渭 <liangwei.lw@nonolive.com>
2023-06-08 14:24:54 +0800

    feat: change sqs message body

--- scripts/post_train.py
+++ scripts/post_train.py
@@ -1,6 +1,8 @@
 import string
 
 import boto3
+import json
+import base64
 
 
 class PostTrainHook:
@@ -19,9 +21,21 @@ class PostTrainHook:
             # todo 这里要指定aws region和aws account
             region = "us-west-2"
             account = "022637123599"
-            # quque_url格式为：f"https://sqs.{region}.amazonaws.com/{account}/sagemaker-hook"
-            quque_url = f"https://sqs.{region}.amazonaws.com/{account}/train_model_job_test"
+            # queue_url格式为：f"https://sqs.{region}.amazonaws.com/{account}/sagemaker-hook"
+            queue_url = f"https://sqs.{region}.amazonaws.com/{account}/train_model_job_test"
+
+        payload_bytes = self._message.encode('utf-8')
+        payload_base64 = base64.b64encode(payload_bytes)
+        real_message = {
+            "biz_type": 0,
+            "topic": "db_train_completed",
+            "metadata": {},
+            "payload": payload_base64.decode('utf-8'),
+            "queue_tag": -1,
+            "key": "",
+        }
+        real_message = json.dumps(real_message)
 
         client = boto3.client('sqs', region_name="us-west-2")
-        response = client.send_message(QueueUrl=quque_url, MessageBody=self._message)
+        response = client.send_message(QueueUrl=queue_url, MessageBody=real_message)
         return response
