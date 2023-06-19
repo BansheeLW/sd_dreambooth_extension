eb5eadde00905252a4a29c750215df814401405e
梁渭 <liangwei.lw@nonolive.com>
2023-06-19 19:12:35 +0800

    feat: 注释掉上传模型的代码（test）

--- scripts/train.py
+++ scripts/train.py
@@ -205,36 +205,36 @@ def train_dreambooth(api_endpoint, train_args, sd_models_s3uri, db_models_s3uri,
         sqs_message = json.dumps(sqs_payload)
         PostTrainHook(sqs_message).to_sqs()
 
-       """  if db_config.use_lora:
-            if username == '':
-                s3uri = f'{lora_models_s3uri}'
-            else:
-                s3uri = f'{lora_models_s3uri}{username}/'
-            print('Uploading Lora Models...')
-            shared.upload_s3files(
-                s3uri,
-                os.path.join(lora_model_dir, f'{db_model_name}_*.pt')
-            )
-
-        #automatic tar latest checkpoint and upload to s3 by zheng on 2023.03.22
-        os.makedirs(os.path.dirname("/opt/ml/model/"), exist_ok=True)
-        os.makedirs(os.path.dirname("/opt/ml/model/Stable-diffusion/"), exist_ok=True)
-        os.makedirs(os.path.dirname("/opt/ml/model/ControlNet/"), exist_ok=True)
-        
-        train_steps=int(db_config.revision)
-        model_file_basename = f'{db_model_name}_{train_steps}_lora' if db_config.use_lora else f'{db_model_name}_{train_steps}'
-        if db_config.v2:
-            f1=os.path.join(sd_models_dir, db_model_name, f'{model_file_basename}.yaml')
-            if os.path.exists(f1):
-                shutil.copy(f1,"/opt/ml/model/Stable-diffusion/")
-        if db_config.save_safetensors:
-            f2=os.path.join(sd_models_dir, db_model_name, f'{model_file_basename}.safetensors')
-            if os.path.exists(f2):
-                shutil.copy(f2,"/opt/ml/model/Stable-diffusion/")
-        else:
-            f2=os.path.join(sd_models_dir, db_model_name, f'{model_file_basename}.ckpt')
-            if os.path.exists(f2):
-                shutil.copy(f2,"/opt/ml/model/Stable-diffusion/") """
+#         if db_config.use_lora:
+#             if username == '':
+#                 s3uri = f'{lora_models_s3uri}'
+#             else:
+#                 s3uri = f'{lora_models_s3uri}{username}/'
+#             print('Uploading Lora Models...')
+#             shared.upload_s3files(
+#                 s3uri,
+#                 os.path.join(lora_model_dir, f'{db_model_name}_*.pt')
+#             )
+#
+#         #automatic tar latest checkpoint and upload to s3 by zheng on 2023.03.22
+#         os.makedirs(os.path.dirname("/opt/ml/model/"), exist_ok=True)
+#         os.makedirs(os.path.dirname("/opt/ml/model/Stable-diffusion/"), exist_ok=True)
+#         os.makedirs(os.path.dirname("/opt/ml/model/ControlNet/"), exist_ok=True)
+#
+#         train_steps=int(db_config.revision)
+#         model_file_basename = f'{db_model_name}_{train_steps}_lora' if db_config.use_lora else f'{db_model_name}_{train_steps}'
+#         if db_config.v2:
+#             f1=os.path.join(sd_models_dir, db_model_name, f'{model_file_basename}.yaml')
+#             if os.path.exists(f1):
+#                 shutil.copy(f1,"/opt/ml/model/Stable-diffusion/")
+#         if db_config.save_safetensors:
+#             f2=os.path.join(sd_models_dir, db_model_name, f'{model_file_basename}.safetensors')
+#             if os.path.exists(f2):
+#                 shutil.copy(f2,"/opt/ml/model/Stable-diffusion/")
+#         else:
+#             f2=os.path.join(sd_models_dir, db_model_name, f'{model_file_basename}.ckpt')
+#             if os.path.exists(f2):
+#                 shutil.copy(f2,"/opt/ml/model/Stable-diffusion/")
     except Exception as e:
         traceback.print_exc()
         print(e)
